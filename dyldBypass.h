dae3994cd399a07cbefffe7ae6006aae74bcd6c0
Billy, The Neighborhood Rascal <54515183+TRGoCPftF@users.noreply.github.com>
2019-10-21 08:10:30 -0400

    Create dyldBypass.h
    Initial Push for Bypass Code

---
+++ dyldBypass.h
@@ -0,0 +1,16 @@
+//
+//  dyldBypass.h
+//
+//  Created by Kyle Nicol on 9/20/19.
+//  Copyright © 2019 Hydra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface dyldBypass : NSObject
++(void)init;
+@end
+
+NS_ASSUME_NONNULL_END
